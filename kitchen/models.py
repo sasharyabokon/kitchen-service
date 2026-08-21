519dd0e8f8075956c6dc9346e0298360836ce983
Sasha <sasha.ryabokon14@gmail.com>
2026-08-21 20:41:42 +0300

    Add Admin classes with fieldsets and update models for admin integration

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="dishes")
+
+    class Meta:
+            ordering = ("name",)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        ordering = ("username",)
--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -126,3 +126,4 @@ MAILERS = {
         "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
 }
+AUTH_USER_MODEL = "kitchen.Cook"
