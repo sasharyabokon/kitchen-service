a44ceae9c18bad30e060156ad4bff496be012d8c
Sasha <sasha.ryabokon14@gmail.com>
2026-09-03 16:49:31 +0300

    Env config setup, add .env.sample, flake8 fixes, set DEFAULT_AUTO_FIELD

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m-p0!rowb)4i1gck+=$qdlv8q3gw99c*1-#j7g7mib5+j@k_fr"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -144,3 +145,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
