a49f28e3d428b8c17e24646cc28ce013ae3af08d
Sasha <sasha.ryabokon14@gmail.com>
2026-09-07 20:57:59 +0300

    Configure environment variables, update root URL routing, and add python-dotenv

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -21,7 +21,7 @@ from kitchen.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("kitchen/", include("kitchen.urls")),
+    path("", include("kitchen.urls")),
     path("", index, name="index"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
