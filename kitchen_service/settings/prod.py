a195f276bd64502249f5c6f4840268756456819d
Sasha <sasha.ryabokon14@gmail.com>
2026-09-08 21:52:36 +0300

    Update production settings and configuration

--- kitchen_service/settings/prod.py
+++ kitchen_service/settings/prod.py
@@ -3,7 +3,7 @@ from kitchen_service.settings.base import *
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["kitchen-service-e4jo.onrender.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
