902a01acbbf9a6b0fde997b8cee5959750ed6448
Sasha <sasha.ryabokon14@gmail.com>
2026-09-08 21:13:36 +0300

    Add Whitenoise, Gunicorn, and build script for Render deployment

--- kitchen_service/settings/prod.py
+++ kitchen_service/settings/prod.py
@@ -5,6 +5,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
